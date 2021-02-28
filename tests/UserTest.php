be7dddaa7583093b6872e8f309980c1c2112ce91
Flavien AUBERT <flavien.aubert.dev@gmail.com>
2021-02-28 14:24:28 +0100

    first test and migration

---
+++ tests/UserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
